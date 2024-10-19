367d938520adfe54970f87a0a0dff6d3003cacd5
r1407p <buszter0403@gmail.com>
2024-10-19 08:41:15 +0000

    feat: add detect video

--- chrome-extension/content-scripts.js
+++ chrome-extension/content-scripts.js
@@ -1,68 +1,79 @@
 console.log("Content script loaded");
 
 // set the minimum width and height for images to be checked
-const minWidth = 300; 
-const minHeight = 300; 
+const minWidth = 200; 
+const minHeight = 200; 
 
 // Function to check all existing images on the page
-function checkAllImages() {
+function checkAllMedia() {
     const images = document.querySelectorAll('img');
-    console.log(`Found ${images.length} images on the page.`);
+    const videos = document.querySelectorAll('video source[type="video/mp4"]');
+    console.log(`Found ${images.length} images and ${videos.length} videos on the page.`);
 
     // Collect all image URLs in a list that meet the size criteria
     const imageElements = Array.from(images).filter(img => img.naturalWidth > minWidth && img.naturalHeight > minHeight);
     const imageUrls = imageElements.map(img => img.src);
     console.log(`Checking image URLs: ${imageUrls}`);
 
-    // Send the list of image URLs to the API
-    checkImagesWithAPI(imageUrls, imageElements);
+    // Collect all video URLs
+    const videoUrls = Array.from(videos).map(video => video.src);
+    console.log(`Checking video URLs: ${videoUrls}`);
+
+    // Send the list of image and video URLs to the API
+    checkMediaWithAPI(imageUrls, videoUrls, imageElements);
 }
 
 // Function to observe changes in the DOM
-function observeNewImages() {
+function observeNewMedia() {
     const observer = new MutationObserver(mutations => {
         let newImageUrls = [];
         let newImageElements = [];
+        let newVideoUrls = [];
 
         mutations.forEach(mutation => {
             mutation.addedNodes.forEach(node => {
                 if (node.tagName === 'IMG') {
-                    if (node.naturalWidth > minWidth && node.naturalHeight > minHeight) {
-                        newImageUrls.push(node.src);
-                        newImageElements.push(node);
-                        console.log(`New image found: ${node.src}`);
+                    node.addEventListener('load', () => {
+                        if (node.naturalWidth > minWidth && node.naturalHeight > minHeight) {
+                            newImageUrls.push(node.src);
+                            newImageElements.push(node);
+                            console.log(`New image found: ${node.src}`);
+                            checkMediaWithAPI(newImageUrls, [], newImageElements);
+                        }
+                    });
+                }
+                if (node.tagName === 'VIDEO') {
+                    const source = node.querySelector('source[type="video/mp4"]');
+                    if (source && source.src) {
+                        newVideoUrls.push(source.src);
+                        console.log(`New video found: ${source.src}`);
+                        checkMediaWithAPI([], newVideoUrls, []);
                     }
                 }
             });
         });
-
-        // Send the new list of image URLs to the API if there are any new images
-        if (newImageUrls.length > 0) {
-            checkImagesWithAPI(newImageUrls, newImageElements);
-            
-        }
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
 }
 
 // Run the initial image check and start observing for new images
-checkAllImages();
-observeNewImages();
+checkAllMedia();
+observeNewMedia();
 
 // Function to check a list of images with the API and update the image elements
-async function checkImagesWithAPI(imageUrls, imgElements) {
+async function checkMediaWithAPI(imageUrls, videoUrls, imgElements) {
     try {
-        const response = await fetch(`https://{ip}:7000/check`, {
+        const response = await fetch(`https://103.156.185.2:7000/check`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ imageUrls: imageUrls }),
+            body: JSON.stringify({ imageUrls: imageUrls, videoUrls: videoUrls }),
         });
 
         const result = await response.json();
-        console.log('Image analysis result:', result);
+        console.log('Media analysis result:', result);
 
         // Loop through the response and update image elements
         imgElements.forEach((imgElement, index) => {
@@ -75,9 +86,9 @@ async function checkImagesWithAPI(imageUrls, imgElements) {
             }
         });
 
-         // Send the analysis data to the background script
-         chrome.runtime.sendMessage({ type: 'imageAnalysisData', data: result });
-         chrome.storage.local.set({ imageAnalysisData: result }, () => {
+        // Send the analysis data to the background script
+        //  chrome.runtime.sendMessage({ type: 'imageAnalysisData', data: result });
+         chrome.storage.local.set({ analysisData: result }, () => {
                 console.log('Analysis data saved to local storage from content script:', result);
             });
          console.log('Sent analysis data to popup:', result);
